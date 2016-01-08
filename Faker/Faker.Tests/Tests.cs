14dc72475a23d2501b779832cf6b81553dc1d9a1
helgy56 <helg56@gmail.com>
2016-01-08 14:36:37 -0600

    Added CA Zip Codes, US Cities, US Countys, and US Zips, and UNits tests for CA provinces

--- Faker/Faker.Tests/Tests.cs
+++ Faker/Faker.Tests/Tests.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text.RegularExpressions;
 
 namespace Faker.Tests
 {
     [TestClass]
     public class Tests
     {
-        
-
         [TestMethod]
         public void StateAbbr()
         {
@@ -17,7 +17,18 @@ namespace Faker.Tests
         public void State()
         {
             Assert.IsNotNull(Address.State());
+        }
 
+        [TestMethod]
+        public void ProvinceAbbr()
+        {
+            Assert.IsTrue(Address.ProvinceAbbreviations().Length == 2);
+        }
+
+        [TestMethod]
+        public void Province()
+        {
+            Assert.IsNotNull(Address.Province());
         }
 
         [TestMethod]
@@ -42,6 +53,29 @@ namespace Faker.Tests
         public void FullName()
         {
             Assert.IsNotNull(Name.FullName());
+        }
+
+        [TestMethod]
+        public void CAZip()
+        {
+            string CaZip = Address.CanadianZipCode();
+            Assert.IsTrue(CaZip.Length == 6);
+            
+            for (int i = 0; i<=5; i++)
+            {
+                if (i == 0)
+                {
+                    Assert.IsFalse(Regex.IsMatch(CaZip[i].ToString(), "[WZ]"));
+                }
+                else if ((i % 2) == 0)
+                {
+                    Assert.IsTrue(Regex.IsMatch(CaZip[i].ToString(), "[ABCEGHJKLMNPRSTVWXYZ]"));
+                }
+                else
+                {
+                    Assert.IsTrue(Regex.IsMatch(CaZip[i].ToString(), "[0-9]"));
+                }
+            }
         }
     }
 }
--- Faker/Faker/Address.cs
+++ Faker/Faker/Address.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Faker
 {
     public static class Address
     {
-
         private static Random random = new Random();
         private static List<string> stateAbbreviations;
         private static List<string> streetName;
         private static List<string> states = null;
-
+        private static List<string> provinceAbbreviations;
+        private static List<string> provinces = null;
 
         public static string State1()
         {
@@ -39,26 +40,63 @@ namespace Faker
             return stateAbbreviations[random.Next(0, stateAbbreviations.Count - 1)];
         }
 
+        public static string Province()
+        {
+
+            if (provinces == null)
+            {
+                provinces = XML.GetListString("Provinces");
+
+            }
+            return provinces[random.Next(0, provinces.Count - 1)];
+        }
+
+        public static string ProvinceAbbreviations()
+        {
+            if (provinceAbbreviations == null)
+            {
+                provinceAbbreviations = XML.GetListString("ProvinceAbbreviations");
+
+            }
+            return provinceAbbreviations[random.Next(0, provinceAbbreviations.Count - 1)];
+        }
+
         public static string StreetName()
         {
             if (streetName == null)
             {
                 streetName = XML.GetListString("StreetName");
-
             }
             return streetName[random.Next(0, streetName.Count - 1)];
         }
 
+        public static string CanadianZipCode()
+        {
+            Random rand = new Random();
+            List<string> alpha = new List<string>{"A", "B", "C", "E", "G", "H", "J", "K", "L", "M", "N",
+            "P", "R", "S", "T", "V", "W", "X", "Y", "Z" };
+            List<int> numeric = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            List<string> alphaFirst = new List<string>{"A", "B", "C", "E", "G", "H", "J", "K", "L", "M", "N",
+            "P", "R", "S", "T", "V", "X", "Y" };
+            StringBuilder newZip = new StringBuilder("");
 
+            for (int i = 0; i <= 5; i++)
+            {
+                if (i == 0)
+                {
+                    newZip.Append(alphaFirst[rand.Next(0, alphaFirst.Count - 1)]);
+                }
+                else if ((i % 2) == 0)
+                {
+                    newZip.Append(alpha[rand.Next(0, alpha.Count - 1)]);
+                }
+                else
+                {
+                    newZip.Append(numeric[rand.Next(0, numeric.Count - 1)]);
+                }
+            }
 
-
-
-
-
-
-
-
-
-
+            return newZip.ToString();
+        }
     }
 }
