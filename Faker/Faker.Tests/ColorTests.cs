171ebc0d7affcabd54297ccc52e87916da6148b4
Jacob Ferm <ferm.jacob@gmail.com>
2016-01-14 00:14:03 -0600

    Stylecop improvements Color class improvements Moving color tests into own file

--- Faker/Faker.Tests/ColorTests.cs
+++ Faker/Faker.Tests/ColorTests.cs
@@ -1,10 +1,9 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="Tests.cs">
+// <copyright file="ColorTests.cs">
 //     Copyright (c) 2016 Jacob Ferm, All rights Reserved
 // </copyright>
 //-----------------------------------------------------------------------
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
 
@@ -12,24 +11,28 @@ namespace Faker.Tests
 {
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "Reviewed")]
     [TestClass]
-    public class Tests
+    public class ColorTests
     {
         [TestMethod]
-        public void MaleFirst()
+        public void ColorRgb()
         {
-            Assert.IsNotNull(Name.MaleFirstName());
+            int i = 0;
+            int[] rgb = Color.RGB();
+            foreach (int code in rgb)
+            {
+                i++;
+                Assert.IsTrue(code >= 0 && code <= 255);
+            }
+
+            Assert.AreEqual(3, i);
         }
 
         [TestMethod]
-        public void FirstName()
+        public void ColorHex()
         {
-            Assert.IsNotNull(Name.FirstName());
-        }
-
-        [TestMethod]
-        public void FullName()
-        {
-            Assert.IsNotNull(Name.FullName());
+            string hex = Color.Hex();
+            Assert.IsTrue(Regex.IsMatch(hex, @"[A-F0-9]+"), "Hex color is: {0}", hex);
+            Assert.AreEqual(hex.Length, 6);
         }
     }
 }
--- Faker/Faker/Color.cs
+++ Faker/Faker/Color.cs
@@ -1,36 +1,46 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Color.cs">
+//     Copyright (c) 2016 Jacob Ferm, All rights Reserved
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Faker
 {
+    /// <summary>
+    /// Static color class
+    /// </summary>
     public static class Color
     {
         private static Random random = new Random();
+
+        /// <summary>
+        /// Get a set of RGB colors 
+        /// </summary>
+        /// <returns>Returns an integer array of numbers 0 - 255</returns>
         public static int[] RGB()
         {
-            int[] rGBColor = new int[] { GenerateColorNumber(), GenerateColorNumber(), GenerateColorNumber() };
-            return rGBColor;
+            int[] rgbColor = new int[] { Number.RandomNumber(0, 255), Number.RandomNumber(0, 255), Number.RandomNumber(0, 255) };
+            return rgbColor;
         }
 
+        /// <summary>
+        /// Get a random HEX number
+        /// </summary>
+        /// <returns>A HEX number as a string</returns>
         public static string Hex()
         {
-            List<string> codes = new List<string>{ "A", "B", "C", "D", "E", "F", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+            List<string> codes = new List<string> { "A", "B", "C", "D", "E", "F", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
             StringBuilder color = new StringBuilder();
 
-            for(int i=0; i<6;i++)
+            for (int i = 0; i < 6; i++)
             {
                 color.Append(codes[random.Next(codes.Count - 1)]);
             }
 
             return color.ToString();
-        }
-
-        public static int GenerateColorNumber()
-        {
-            return random.Next(0, 255);
         }
     }
 }
