277c82a95269797a9e4bc41d711e27a7a09b480e
Jacob Ferm <ferm.jacob@gmail.com>
2016-03-02 00:21:58 -0600

    Adding date class.  No code

---
+++ Faker/Faker/Date.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Date.cs">
+//     Copyright (c) 2016 Jacob Ferm, All rights Reserved
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    /// <summary>
+    /// Class for date generation
+    /// </summary>
+    public static class Date
+    {
+        // Get Past
+        // Get Past (year)
+        // Get Future
+        // Get Future (year)
+        // Get Recent (day span)
+        // Get Date Between (from, to)
+    }
+}
